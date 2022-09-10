785c8b25c66edcb274de516565cad4aa5b92f67d
Shane Wilson <shanewilsonpro@gmail.com>
2022-09-10 14:31:06 -0700

    fixed bugs and setup and connected mysql database to project

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express();
-
 const errorController = require("./controllers/error");
+const db = require("./util/database");
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
