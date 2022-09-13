0d29e997ec2f8d1cda6408edc778eaab5d9f4ddc
Shane Wilson <shanewilsonpro@gmail.com>
2022-09-13 15:46:22 -0700

    using sequelize for database, added new models, and updated functionalities on products and orders

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: "root",
-    database: 'junk-shop',
-    password: ''
+const sequelize = new Sequelize("junk-shop", "root", "", {
+  dialect: "mysql",
+  host: "localhost",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
